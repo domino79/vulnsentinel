3c545e2515fd946a4e42a0b5946582a4a2f5a776
domino79 <160327819+domino79@users.noreply.github.com>
2025-04-24 01:37:43 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 from parser.log_parser import scan_log
 
@@ -8,8 +8,6 @@ ALLOWED_EXTENSIONS = {'txt', 'log'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Ensure the upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def allowed_file(filename):
@@ -22,11 +20,11 @@ def dashboard():
     if request.method == 'POST':
         if 'logfile' not in request.files:
             return redirect(request.url)
-        
+
         file = request.files['logfile']
         if file.filename == '':
             return redirect(request.url)
-        
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
